4a1624606721507adcb8da1cdc670a65a269c88b
Camelia Rasiga <rasigacamelia@gmail.com>
2023-10-22 15:42:34 +0300

    Add project files.

---
+++ Data/Rasiga_Camelia_Lab2Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Rasiga_Camelia_Lab2.Models;
+
+namespace Rasiga_Camelia_Lab2.Data
+{
+    public class Rasiga_Camelia_Lab2Context : DbContext
+    {
+        public Rasiga_Camelia_Lab2Context (DbContextOptions<Rasiga_Camelia_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Rasiga_Camelia_Lab2.Models.Book> Book { get; set; } = default!;
+    }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,13 @@
+﻿namespace Rasiga_Camelia_Lab2.Models
+{
+    public class Book
+    {
+        public string ID {  get; set; }
+
+        public string Title {  get; set; }
+
+        public string Author {  get; set; }
+
+        public decimal Price {  get; set; }
+    }
+}
